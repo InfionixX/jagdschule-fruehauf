2827800f76ebab1dc0d0b8d109874848def0515f
InfionixX <jonaskahle@gmx.de>
2025-03-22 15:37:13 +0100

    colors adjusted and Pop Ups added

---
+++ _scripts/popup.js
@@ -0,0 +1,7 @@
+function closePopup(popup_id) {
+    document.getElementById(popup_id).style.display = 'none';
+}
+
+function openPopup(popup_id){
+    document.getElementById(popup_id).style.display = 'flex';
+}
