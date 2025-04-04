a24bbb4bbe8511e9c339fb33cd8bd4ab6d104f2b
Kahle <jonas.kahle@siemens.com>
2025-04-04 16:31:35 +0200

    added animations and altered controls

---
+++ _scripts/animations.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const fadeElements = document.querySelectorAll(".fade-in");
+
+    function checkVisibility() {
+        fadeElements.forEach((element) => {
+            const rect = element.getBoundingClientRect();
+            if (rect.top < window.innerHeight * 0.85) {
+                element.classList.add("visible");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", checkVisibility);
+    checkVisibility();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const fadeElements = document.querySelectorAll(".fade-in-training");
+
+    function checkVisibility() {
+        fadeElements.forEach((element) => {
+            const rect = element.getBoundingClientRect();
+            if (rect.top < window.innerHeight * 0.85) {
+                    element.classList.add("visible");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", checkVisibility);
+    checkVisibility();
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const profiles = document.querySelectorAll(".profile");
+  
+    // Intersection Observer für jedes Bild
+    const observer = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach((entry, index) => {
+          if (entry.isIntersecting) {
+            // Bild nach einer Verzögerung anzeigen
+            setTimeout(() => {
+              entry.target.classList.add("visible");
+            }, index * 500); // Verzögerung von 0.5 Sekunden für jedes Bild
+          }
+        });
+      },
+      { threshold: 0.3 } // 30% der Bilder müssen sichtbar sein
+    );
+  
+    profiles.forEach(profile => {
+      observer.observe(profile); // Jedes Bild wird überwacht
+    });
+  });
